345c70ff25b9acf13039f4b0930a5f13d1cfbb1c
somnath <somnathmishra100dbi@gmail.com>
2024-09-12 00:32:57 +0530

    fix messages p-1

--- src/app/api/messages/unread-count/route.ts
+++ src/app/api/messages/unread-count/route.ts
@@ -1,26 +1,26 @@
-// import { validateRequest } from "@/auth";
-// import streamServerClient from "@/lib/stream";
-// import { MessageCountInfo } from "@/lib/types";
+import { validateRequest } from "@/auth";
+import streamServerClient from "@/lib/stream";
+import { MessageCountInfo } from "@/lib/types";
 
-// export async function GET() {
-//   try {
-//     const { user } = await validateRequest();
+export async function GET() {
+  try {
+    const { user } = await validateRequest();
 
-//     if (!user) {
-//       return Response.json({ error: "Unauthorized" }, { status: 401 });
-//     }
+    if (!user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-//     const { total_unread_count } = await streamServerClient.getUnreadCount(
-//       user.id,
-//     );
+    const { total_unread_count } = await streamServerClient.getUnreadCount(
+      user.id,
+    );
 
-//     const data: MessageCountInfo = {
-//       unreadCount: total_unread_count,
-//     };
+    const data: MessageCountInfo = {
+      unreadCount: total_unread_count,
+    };
 
-//     return Response.json(data);
-//   } catch (error) {
-//     console.error(error);
-//     return Response.json({ error: "Internal server error" }, { status: 500 });
-//   }
-// }
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
